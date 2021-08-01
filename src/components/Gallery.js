6a2d8da0595562d060499ed9c40549458e2b628a
Amrit Thapa <amet.thapa27@gmail.com>
2021-08-01 18:30:31 +0545

    complete responsive

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,28 +1,28 @@
-// import img1 from '../images/1.png'
-// import img2 from '../images/2.jpg'
-// import img3 from '../images/3.jpg'
+import img1 from '../images/1.png'
+import img2 from '../images/2.jpg'
+import img3 from '../images/3.jpg'
 
 
 
-const Gallery = () =>{
+const Gallery = () => {
     return(
         <div className="gallery">
-            <div className="photo img1">
-                {/* <img src={img1} alt="phone"></img> */}
+            <div className="photo">
+                <img src={img1} alt="phone"></img>
                 <div className="gallery-text">
                     <h3>Mobile App</h3>
                     <h2>Ubar Mobile App</h2>
                 </div>
             </div>
-            <div className="photo img2">
-                {/* <img src={img2} alt="phone"></img> */}
+            <div className="photo ">
+                <img src={img2} alt="phone"></img>
                 <div className="gallery-text">
                     <h3>Mobile App</h3>
                     <h2>Ubar Mobile App</h2>
                 </div>
             </div>
-            <div className="photo img3">
-                {/* <img src={img3} alt="phone"></img> */}
+            <div className="photo">
+                <img src={img3} alt="phone"></img>
                 <div className="gallery-text">
                     <h3>Mobile App</h3>
                     <h2>Ubar Mobile App</h2>
